6cf38995e38105478228336f2be6452a03c9c633
deepak <deepakchandrasekaran14@gmail.com>
2023-11-21 16:13:18 +0530

    logout functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,32 @@
 import NavLogo from "../../src/logo.jpg";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../store/userSlice";
+
 const Navbar = () => {
+    const user = useSelector((state) => state.user);
+    const getUserFromSessionStorage = JSON.parse(sessionStorage.getItem('user'));
+    const dispatch = useDispatch();
+
+    const handlerLogout = () => {
+        sessionStorage.clear();
+        dispatch(remove());
+    }
+
     return (
         <div className="w-full border border-gray-300">
-            <nav>
+            <nav className="flex items-center justify-between">
                 <a className="flex items-center w-[180px]">
                     <img className="h-13 w-16" alt="nav-logo" src={NavLogo} />
                     <span className="pt-2 text-sm">Reimbursement Tracker</span>
                 </a>
-                {/* <ul>
-                    <li><a>Sign Up</a></li>
-                    <li><a>Login</a></li>
-                </ul> */}
+                <ul className="border border-gray-300 mr-5">
+                    {
+                        (user?.uId || getUserFromSessionStorage?.uId) &&
+                        (
+                            <li><button onClick={handlerLogout} className="px-3 py-1 text-sm rounded-md hover:text-red-600">Log out</button></li>
+                        )
+                    }
+                </ul>
             </nav>
         </div>
     )
