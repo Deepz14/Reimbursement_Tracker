080cfd5f013e0a04e371208cb78b6af12b4d4789
deepak <deepakchandrasekaran14@gmail.com>
2023-11-21 14:48:08 +0530

    protected route hook added

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import Authentication from './Authentication';
 import Navbar from './Navbar';
 import SignUp from "./Signup";
 import Login from "./Login";
 import Expenses from './Expenses';
+import ProtectedRoute from "../hooks/useProtectedRoute";
 
 const Body = () => {
     const userInfo = useSelector((state) => state.user);
@@ -14,7 +14,9 @@ const Body = () => {
             <Navbar />
             <Router>
                 <Routes>
-                    <Route exact path="/" element={<Expenses />}></Route>
+                    <Route element={<ProtectedRoute />}>
+                        <Route exact path="/" element={<Expenses />}></Route>
+                    </Route>
                     <Route element={<Authentication />} path="auth">
                         <Route index element={<SignUp />} />
                         <Route element={<SignUp />} path='signup' />
