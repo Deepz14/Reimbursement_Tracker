9e529d8bd52d0d30506d552b66d98104436ef017
deepak <deepakchandrasekaran14@gmail.com>
2023-11-21 13:09:58 +0530

    code update

--- frontend/src/components/Authentication.js
+++ frontend/src/components/Authentication.js
@@ -1,4 +1,4 @@
-import Login from "./Login"
+// import Login from "./Login"
 import SignUp from "./Signup"
 
 const Authentication = () => {
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -21,7 +21,7 @@ const SignUp = () => {
     }
 
     return (
-        <div className="border border-black-300 rounded mt-20 relative top-10 md:w-1/3 md:mx-auto">
+        <div className="border border-black-300 rounded mt-20 relative top-5 md:w-1/3 md:mx-auto">
             <h1 className="font-bold text-2xl text-center my-5 md:text-xl">Sign Up</h1>
             <div className="my-5 py-2 mx-3 px-3">
                 <input ref={name} className="border border-gray-300 w-full p-3" type="text" placeholder="Enter name" />
