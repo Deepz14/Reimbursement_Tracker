4e06a0dfa9fa51410e22b716a2c495a8f5dc5d3a
deepak <deepakchandrasekaran14@gmail.com>
2023-11-26 19:42:24 +0530

    minor change

--- hr-reimbursement/src/components/NewAccount.js
+++ hr-reimbursement/src/components/NewAccount.js
@@ -48,7 +48,7 @@ const NewAccount = () => {
         }else{
             if(response?.success){
                 setLoader(false);
-                window.location.replace(process.env.REACT_APP_HR_ACCOUNT_CREATION_REDIRECTION + "/auth");
+                window.location.assign(process.env.REACT_APP_HR_ACCOUNT_CREATION_REDIRECTION + "/auth");
                 //showSuccessPrompt('New Account has been created!');
             }
         }
